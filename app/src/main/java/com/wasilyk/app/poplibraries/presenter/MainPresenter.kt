5d74fa18b3438bbebac6b38edce5429f49f652a9
WasilyK <vasua84@yandex.ru>
2021-07-27 15:22:18 +0900

    fixed presenter error =)

--- app/src/main/java/com/wasilyk/app/poplibraries/presenter/MainPresenter.kt
+++ app/src/main/java/com/wasilyk/app/poplibraries/presenter/MainPresenter.kt
@@ -7,6 +7,6 @@ class MainPresenter(private val view: MainView) {
     private val model = CountersModel()
 
     fun counterButton1Click() = view.setButton1Text("${model.next(0)}")
-    fun counterButton2Click() = view.setButton1Text("${model.next(1)}")
-    fun counterButton3Click() = view.setButton1Text("${model.next(2)}")
+    fun counterButton2Click() = view.setButton2Text("${model.next(1)}")
+    fun counterButton3Click() = view.setButton3Text("${model.next(2)}")
 }
